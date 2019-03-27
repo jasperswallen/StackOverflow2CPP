0eb6422540e8861ea0b1ba337b9b515a6021f7b1
Jasper Swallen <swallen@usc.edu>
2019-03-26 17:42:11 -0700

    cleaner background (removed comments)

--- background.js
+++ background.js
@@ -1,22 +1,20 @@
-console.log("Loaded extension...");
-
-/*chrome.webRequest.onBeforeRequest.addListener(
-    function() { return {cancel: true}; },
-    {
-      urls: ["*://cdn.sstatic.net/Sites/stackoverflow/*"],
-      types: ["script"]
-    },
-    ["blocking"]
-  );*/
+console.log('Loaded extension...');
 
 function blockRequest(details) {
-    return {cancel: true};
+  return {cancel: true};
 }
 
 function updateFilters(urls) {
-    if(chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
-        chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
-    chrome.webRequest.onBeforeRequest.addListener(blockRequest, {urls: ["*://cdn.sstatic.net/Sites/stackoverflow/*", "*://cdn.sstatic.net/Shared/*"]}, ['blocking']);
+  if (chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
+    chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
+  chrome.webRequest.onBeforeRequest.addListener(
+      blockRequest, {
+        urls: [
+          '*://cdn.sstatic.net/Sites/stackoverflow/*',
+          '*://cdn.sstatic.net/Shared/*'
+        ]
+      },
+      ['blocking']);
 }
 
 updateFilters();
